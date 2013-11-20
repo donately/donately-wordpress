2e6ecabc159584704d0bb62d51714ed695ffba89
bryanmonzon <monzonb@gmail.com>
2013-11-20 14:57:00 -0800

    initial commit

---
+++ donately.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Plugin Name: Donately for WordPress
+ * Plugin URI: http://labs.fiftyandfifty.org
+ * Description: Utilize the processing power of Donately with the flexibility of WordPress for your organizations donations.
+ * Version: 4.0
+ * Author: Fifty and Fifty
+ * Author URI: http://fiftyandfifty.org
+ */
