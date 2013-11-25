aa28c62614f7e0e50b5809fc401d992bde8e4b73
Alexander Zizzo <elzi@nerdi.net>
2013-11-25 15:57:41 -0800

    dntly-helpers part 1 done

--- lib/dntly-helpers.php
+++ lib/dntly-helpers.php
@@ -22,18 +22,60 @@ class DNTLY_FIELDS {
     $this->dntly_environment  = get_post_meta($post->ID, '_dntly_environment', true);
   }
   
-  function dntly_data(){
+
+
+  /**
+   * DNTLY Data
+   *
+   * @since 0.1
+   * @package Donately Wordpress
+   * @author Alexander Zizzo, Bryan Shanaver, Bryan Monzon (Fifty and Fifty, LLC)
+   * @return [array] '_dntly_data' post meta
+   */
+  function dntly_data( $args = NULL )
+  {
     return $this->dntly_data;
   }
-  function dntly_environment(){
+
+
+  /**
+     * DNTLY Environment
+     *
+     * @since 0.1
+     * @package Donately Wordpress
+     * @author Alexander Zizzo, Bryan Shanaver, Bryan Monzon (Fifty and Fifty, LLC)
+     * @return [array] '_dntly_environment' post meta
+     */
+  function dntly_environment( $args = NULL )
+  {
     return $this->dntly_environment;
   }
 
-  function dntly_account_id(){
+
+
+  /**
+     * DNTLY Account ID
+     *
+     * @since 0.1
+     * @package Donately Wordpress
+     * @author Alexander Zizzo, Bryan Shanaver, Bryan Monzon (Fifty and Fifty, LLC)
+     * @return [array] '_dntly_account_id' post meta
+     */
+  function dntly_account_id( $args = NULL )
+  {
     return $this->dntly_account_id;
   }
 
-  function dntly_account_title(){
+  /**
+     * DNTLY Account Title
+     *
+     * @since 0.1
+     * @package Donately Wordpress
+     * @author Alexander Zizzo, Bryan Shanaver, Bryan Monzon (Fifty and Fifty, LLC)
+     * @return (string) 'account_title' option value
+     */
+  function dntly_account_title( $args = NULL )
+  {
     if ( isset($this->dntly_data['account_title']) ) {
       $account_title = $this->dntly_data['account_title'];
     } else {
@@ -42,15 +84,37 @@ class DNTLY_FIELDS {
     return $account_title;
   }
 
-  function dntly_campaign_id() {
+  /**
+     * DNTLY Campaign ID
+     *
+     * @since 0.1
+     * @package Donately Wordpress
+     * @author Alexander Zizzo, Bryan Shanaver, Bryan Monzon (Fifty and Fifty, LLC)
+     * @return [array] '_dntly_campaign_id' post meta
+     */
+  function dntly_campaign_id( $args = NULL )
+  {
     return $this->dntly_camp_id;
   }
-  function dntly_parent_campaign_id(){
-    return $this->dntly_campaign_id;
-  }
 
 
-  function dntly_campaign_goal() {      
+
+  /**
+     * DNTLY Campaign Goal
+     *
+     * @since 0.1
+     * @package Donately Wordpress
+     * @author Alexander Zizzo, Bryan Shanaver, Bryan Monzon (Fifty and Fifty, LLC)
+     * @return (int) $campaign_goal || (string) formatted USD $campaign_goal
+     */
+  function dntly_campaign_goal( $args = NULL )
+  {      
+    // Parameters
+    $format   = isset($args['format']) ? $args['format'] : null;
+    $locale   = isset($args['locale']) ? $args['locale'] : 'en_US';
+    $currency = isset($args['currency']) ? $args['currency'] : '%i';
+
+    // If 'campaign_goal' is set, set $campaign_goal as it's intval, else return 0
     if ( isset($this->dntly_data['campaign_goal']) ) {
       // if it is set and not NULL, get campaign goal integar.
       $campaign_goal = intval($this->dntly_data['campaign_goal']);
@@ -58,62 +122,119 @@ class DNTLY_FIELDS {
       // otherwise set it to zero.
       $campaign_goal = intval(0);
     }
-    return $campaign_goal;
-  }
 
-  function dntly_fundraiser_goal() {      
-    if ( isset($this->dntly_data['goal']) ) {
-      // if it is set and not NULL, get campaign goal integar.
-      $goal = intval($this->dntly_data['goal']);
+    // If format parameter is passed
+    if ( $format ) {
+      // Set locale
+      setlocale(LC_MONETARY, $locale);
+      // Format to USD
+      $campaign_goal_formatted = money_format($currency, $campaign_goal);
+      // Return the formatted goal integer
+      return $campaign_goal_formatted;
     } else {
-      // otherwise set it to zero.
-      $goal = intval(0);
+      // Return unformatted goal integer
+      return $campaign_goal;
     }
-    return $goal;
   }
 
-  function dntly_donations_count() {      
+
+
+
+
+  /**
+     * DNTLY Donations Count
+     *
+     * @since 0.1
+     * @package Donately Wordpress
+     * @author Alexander Zizzo, Bryan Shanaver, Bryan Monzon (Fifty and Fifty, LLC)
+     * @return (string) 'donations_count'
+     */
+  function dntly_donations_count( $args = NULL )
+  {     
+    // If it is set and not NULL, get campaign goal integar.
     if ( isset($this->dntly_data['donations_count']) ) {
-      // if it is set and not NULL, get campaign goal integar.
       $donations_count = intval($this->dntly_data['donations_count']);
-    } else {
-      // otherwise set it to zero.
+    } 
+    // Otherwise set it to zero.
+    else {
       $donations_count = intval(0);
     }
     return $donations_count;
   }
 
 
-  function dntly_donors_count() {      
 
+
+
+
+  /**
+     * DNTLY Donors Count
+     *
+     * @since 0.1
+     * @package Donately Wordpress
+     * @author Alexander Zizzo, Bryan Shanaver, Bryan Monzon (Fifty and Fifty, LLC)
+     * @return (int) $donors_count
+     */
+  function dntly_donors_count( $args = NULL )
+  {      
+    // If it is set and not NULL, get campaign goal integar.
     if ( isset($this->dntly_data['donors_count']) ) {
-      // if it is set and not NULL, get campaign goal integar.
       $donors_count = intval($this->dntly_data['donors_count']);
-    } else {
-      // otherwise set it to zero.
+    } 
+    // Otherwise set it to zero.
+    else {
       $donors_count = intval(0);
     }
     return $donors_count;
   }
 
 
-  function dntly_amount_raised() {      
+  /**
+     * DNTLY Amount Raised
+     *
+     * @since 0.1
+     * @package Donately Wordpress
+     * @author Alexander Zizzo, Bryan Shanaver, Bryan Monzon (Fifty and Fifty, LLC)
+     * @return (int) $amount_raised
+     */
+  function dntly_amount_raised( $args = NULL ) {
+
+    // !! See dntly_campaign_goal for comments !!
+
+    $format   = isset($args['format']) ? $args['format'] : null;
+    $locale   = isset($args['locale']) ? $args['locale'] : 'en_US';
+    $currency = isset($args['currency']) ? $args['currency'] : '%i';  
 
     if ( isset($this->dntly_data['amount_raised']) ) {
-      // if it is set and not NULL, get fundraiser amount raised integar.
       $amount_raised = intval($this->dntly_data['amount_raised']);
     } else {
-      // otherwise set it to zero.
       $amount_raised = intval(0);
     }
-    return $amount_raised;
+
+    if ( $format ) {
+      setlocale(LC_MONETARY, $locale);
+      $amount_raised_formatted = money_format($currency, $amount_raised);
+      return $amount_raised_formatted;
+    } else {
+      return $amount_raised;
+    }
+
   }
 
-  function dntly_percentage_raised() {
+  /**
+     * DNTLY Percentage Raised
+     *
+     * @since 0.1
+     * @package Donately Wordpress
+     * @author Alexander Zizzo, Bryan Shanaver, Bryan Monzon (Fifty and Fifty, LLC)
+     * @return (int) $percentage_raised
+     */
+  function dntly_percentage_raised( $args = NULL ) {
 
+    // Vars
     $campaign_goal = $this->dntly_campaign_goal();
     $amount_raised = $this->dntly_amount_raised();
-
+ 
     if ( $campaign_goal ) {
       $percentage_raised = number_format( $amount_raised / $campaign_goal * 100 );
     } else {
@@ -123,21 +244,19 @@ class DNTLY_FIELDS {
     return $percentage_raised;
   }
 
-  function dntly_fundraiser_percentage_raised() {
+ 
 
-    $fundraiser_goal = $this->dntly_fundraiser_goal();
-    $amount_raised   = $this->dntly_amount_raised();
 
-    if ( $fundraiser_goal ) {
-      $percentage_raised = number_format( $amount_raised / $fundraiser_goal * 100 );
-    } else {
-      $percentage_raised = intval(0);
-    }
 
-    return $percentage_raised;
-  }
 
-  // META GENERAL
+  /**
+     * DNTLY Meta General
+     *
+     * @since 0.1
+     * @package Donately Wordpress
+     * @author Alexander Zizzo, Bryan Shanaver, Bryan Monzon (Fifty and Fifty, LLC)
+     * @return [array]
+     */
   function dntly_meta_general() {
 
     if ( isset($post) && is_object($post) && isset($post->ID) && !empty($post->ID)) {
@@ -145,32 +264,11 @@ class DNTLY_FIELDS {
     } else {
       $meta_general = null;
     }
-
     return $meta_general;
   }
 
-  // TRACKING_CODES
-  function dntly_tracking_codes() {
-
-    if ( isset($this->dntly_data['tracking_codes']) ) {
-     $tracking_codes = $this->dntly_data['tracking_codes'];
-    } else {
-     $tracking_codes = null;
-      // $track_codes = var_dump($this->dntly_data['tracking_codes']);
-    }
-    return $tracking_codes;
-  }
 
 }
-
-
-
-
-
-
-
-
-
 
 
 
@@ -187,25 +285,30 @@ class DNTLY_FIELDS {
 
 add_action('init', 'dntly_helper_functions');
 
-function dntly_helper_functions(){
+function dntly_helper_functions()
+{
 
   /* ACCOUNT ID
   ================================================== */
-  function get_the_account_id(){
+  function get_the_account_id()
+  {
     $df = new Dntly_fields;
     return $df->dntly_account_id;
   }
-  function dntly_account_id(){
+  function dntly_account_id()
+  {
     echo get_the_account_id();
   }
 
   /* ACCOUNT TITLE
   ================================================== */
-  function get_the_account_title(){
+  function get_the_account_title()
+  {
     $df = new Dntly_fields;
     return $df->dntly_account_title;
   }
-  function account_title(){
+  function account_title()
+  {
     echo get_the_account_title();
   }
 
@@ -230,11 +333,13 @@ function dntly_helper_functions(){
   }
   /* CAMPAIGN GOAL
   ================================================== */
-  function get_the_campaign_goal(){
+  function get_the_campaign_goal()
+  {
     $df = new Dntly_Fields;
     return $df->dntly_campaign_goal();
   }
-  function campaign_goal(){
+  function campaign_goal()
+  {
 
     $campaign_goal      = get_the_campaign_goal();
     $campaign_goal_usd  = number_format( $campaign_goal );
@@ -244,21 +349,25 @@ function dntly_helper_functions(){
 
   /* FUNDRAISER ID
   ================================================== */
-  function get_the_fundraiser_id(){
+  function get_the_fundraiser_id()
+  {
     return get_the_campaign_id();
   }
-  function fundraiser_id(){
+  function fundraiser_id()
+  {
     echo campaign_id();
   }
 
 
   /* FUNDRAISER GOAL
   ================================================== */
-  function get_the_fundraiser_goal(){
+  function get_the_fundraiser_goal()
+  {
     $df = new Dntly_Fields;
     return $df->dntly_fundraiser_goal();
   }
-  function fundraiser_goal(){
+  function fundraiser_goal()
+  {
 
     $fundraiser_goal      = get_the_fundraiser_goal();
     $fundraiser_goal_usd  = number_format( $fundraiser_goal );
@@ -268,27 +377,32 @@ function dntly_helper_functions(){
 
   /* DONATION COUNT
   ================================================== */
-  function get_the_donations_count(){
+  function get_the_donations_count()
+  {
     $df = new Dntly_Fields;
     return $df->dntly_donations_count();
   }
-  function donations_count(){
+  function donations_count()
+  {
     echo get_the_donations_count();
   }
 
   /* DONORS COUNT
   ================================================== */
-  function get_the_donors_count(){
+  function get_the_donors_count()
+  {
     $df = new Dntly_Fields;
     return $df->dntly_donors_count();
   }
-  function donors_count(){
+  function donors_count()
+  {
     echo get_the_donors_count();
   }
 
   /* AMOUNT RAISED
   ================================================== */
-  function get_the_amount_raised(){
+  function get_the_amount_raised()
+  {
     $df = new Dntly_Fields;
     return $df->dntly_amount_raised();
   }
@@ -304,7 +418,8 @@ function dntly_helper_functions(){
 
   /* CAMPAIGN PERCENTAGE RAISED
   ================================================== */
-  function get_the_percentage_raised(){
+  function get_the_percentage_raised()
+  {
     $df = new Dntly_Fields;
     return $df->dntly_percentage_raised();
   }
@@ -314,31 +429,37 @@ function dntly_helper_functions(){
 
   /* FUNDRAISER PERCENTAGE RAISED
   ================================================== */
-  function get_the_fundraiser_percentage_raised(){
+  function get_the_fundraiser_percentage_raised()
+  {
     $df = new Dntly_Fields;
     return $df->dntly_fundraiser_percentage_raised();
   }
-  function fundraiser_percentage_raised(){
+  function fundraiser_percentage_raised()
+  {
     echo get_the_fundraiser_percentage_raised();
   }
 
   /* META GENERAL
   ================================================== */
-  function get_the_meta_general(){
+  function get_the_meta_general()
+  {
     $df = new Dntly_fields;
     return $df->dntly_meta_general();
   }
-  function meta_general(){
+  function meta_general()
+  {
     echo get_the_meta_general();
   }
 
   /* TRACKING CODES
   ================================================== */
-  function get_the_tracking_codes(){
+  function get_the_tracking_codes()
+  {
     $df = new Dntly_fields;
     return $df->dntly_tracking_codes();
   }
-  function dntly_tracking_codes(){
+  function dntly_tracking_codes()
+  {
     echo get_the_tracking_codes();
   }
 
