3a9b3ad2a4fc16a94a2dd9b1b51cdd70777d4f43
bryanmonzon <monzonb@gmail.com>
2013-11-29 15:16:12 -0800

    remove shortcodes

--- lib/dntly-shortcodes.php
+++ lib/dntly-shortcodes.php
@@ -1,2 +1,13 @@
 <?php
+/**
+ * Shortcodes
+ *
+ * @package     Donately
+ * @subpackage  Shortcodes
+ * @copyright   Copyright (c) 2013, Fifty and Fifty
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.0
+ */
 
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
