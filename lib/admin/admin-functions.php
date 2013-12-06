36bba21bc812e7e739aa89e0e6733950cb31a396
bryanmonzon <monzonb@gmail.com>
2013-12-06 13:44:49 -0800

    admin-functions

---
+++ lib/admin/admin-functions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Admin Functions
+ */
+
+    
+    $dntly_settings_page = isset( $_GET['page'] ) ? $_GET['page'] : '';
+    $tab_name            = isset( $_GET['tab'] ) ? $_GET['tab'] : '';
+    $settings_updated    = isset( $_GET['settings-updated'] ) ? $_GET['settings-updated'] : false;
+
+    // if( $dntly_settings_page = 'dntly-settings') {
+    //     echo 'Page: ' . $dntly_settings_page . '<br>';
+    // }   
+    // if( $tab_name ) {
+    //     echo 'Tab: ' . $tab_name . '<br>';
+    // }
+    // if( $settings_updated ) {
+    //     echo 'Settings Update: ' . $settings_updated;
+    // }
+
+    if( $dntly_settings_page == 'dntly-settings' && $settings_updated == true  ) {
+        ob_start(); ?>
+        
+        <p>Guacamole</p>
+
+        <?php
+        echo ob_get_clean();
+    }
