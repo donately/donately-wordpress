8ab128d97352a3e2499acc2c006f1ac4cc44952e
bryanmonzon <monzonb@gmail.com>
2013-12-06 17:14:27 -0800

    started work on donately_form()

--- lib/donations/functions.php
+++ lib/donations/functions.php
@@ -55,8 +55,82 @@ function dntly_get_donation_page_uri( $args = array() ) {
  * @param  [type] $post [description]
  * @return [type]       [description]
  */
-function donately_form( $post )
+function donately_form( $post_id)
 {
     global $post, $dntly_settings;
 
+    $dntly = new DNTLY_API;
+    $form_js_url = 'http://'. $dntly->api_subdomain . '.dntly.com' . '/assets/js/v1/form.js';  //Needs to be refactored to included environment variables.
+
+    $cid = ( isset( $_GET['cid'] ) ) ? $_GET['cid'] : dntly_get_campaign_id();
+    ob_start();
+    ?>
+    <ul>
+        <li>Dntly Form JS: <strong><?php echo $form_js_url; ?></strong></li>
+        <li>Account ID: <strong><?php echo dntly_get_account_id( $cid ); ?></strong></li>
+        <li>Campaign ID: <strong><?php echo $cid; ?></strong></li>
+        <li>Campagin Goal: <strong><?php echo dntly_get_campaign_goal(); ?></strong></li>
+        <li>Campaign Raised: <strong><?php echo dntly_get_amount_raised(); ?></strong></li>
+        <li>Percent Funded: <strong><?php echo dntly_get_percent_funded(); ?></strong></li>
+    </ul>
+    
+   <script class="donately-formjs" src='<?php echo $form_js_url; ?>' type='text/javascript' async='async'
+      data-donately-id='<?php //echo dntly_get_account_id(); ?>198'
+      data-donately-campaign-id='<?php echo $cid; ?>'
+     ></script>
+    <?php
+    $dntly_form = ob_get_clean();
+    echo $dntly_form;
+
 }
+
+
+function dntly_get_campaign_id()
+{
+    global $post;
+
+    $dntly_campaign_meta = get_post_meta( $post->ID, 'dntly_campaign_id', true );
+
+    return $dntly_campaign_meta;
+}
+
+function dntly_get_account_id()
+{
+    global $post;
+
+    $dntly_account_id = get_post_meta( $post->ID, 'dntly_account_id', true );
+
+    return $dntly_account_id;
+}
+
+function dntly_get_campaign_goal()
+{
+    global $post;
+
+    $dntly_campaign_goal = get_post_meta( $post->ID, 'dntly_campaign_goal', true );
+
+    return '$'.number_format( $dntly_campaign_goal );
+}
+
+function dntly_get_amount_raised()
+{
+    global $post;
+
+    $dntly_amount_raised = get_post_meta( $post->ID, 'dntly_amount_raised', true );
+    $dntly_amount_raised = isset( $dntly_amount_raised ) ? '$' . $dntly_amount_raised : 0;
+
+    return $dntly_amount_raised;
+}
+
+function dntly_get_percent_funded()
+{
+    global $post;
+
+    $dntly_percent_funded = get_post_meta( $post->ID, 'dntly_percent_funded', true );
+    $dntly_percent_funded = round((float)$dntly_percent_funded * 100 ) . '%';
+
+    return $dntly_percent_funded;
+}
+
+
+
